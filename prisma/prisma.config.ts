e16ece01b733a0630f48ced290efd99b23755a9f
nikosthanos-cloud <nikosthanos@gmail.com>
2026-04-02 16:54:21 +0300

    Remove problematic prisma.config.ts

--- prisma/prisma.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "@prisma/internals";
-
-export default defineConfig({
-  // For Prisma 7, connection URLs are passed to PrismaClient constructor
-  // See: https://pris.ly/d/prisma7-client-config
-});
